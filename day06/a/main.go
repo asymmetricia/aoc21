0b150764341fb89ae3f0fbf3a42b99f3402e6124
asymmetricia <tricia-github@cernu.us>
2021-12-01 17:11:30 -0800

    skels

---
+++ day06/a/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"io/ioutil"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+)
+
+var log = logrus.StandardLogger()
+
+func main() {
+	input, err := ioutil.ReadFile("input")
+	if err != nil {
+		log.WithError(err).Fatal("could not read input")
+	}
+
+	lines := strings.Split(strings.TrimSpace(string(input)), "\n")
+
+	for _, line := range lines {
+		line = line
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/asymmetricia/aoc21
 
 go 1.16
+
+require github.com/sirupsen/logrus v1.8.1 // indirect
