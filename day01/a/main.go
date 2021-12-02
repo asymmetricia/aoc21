bf32532f0938273ce71e865508620e42240a7ec3
asymmetricia <tricia-github@cernu.us>
2021-12-01 17:02:41 -0800

    day 1 A

---
+++ day01/a/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	in, err := ioutil.ReadFile("input")
+	if err != nil {
+		panic(err)
+	}
+	lines := strings.Split(string(in), "\n")
+
+	var prev *int
+	count:= 0
+	for _, line := range lines {
+		if prev == nil {
+			prev = new(int)
+			*prev, _ = strconv.Atoi(line)
+		} else {
+			this, _ := strconv.Atoi(line)
+			if this > *prev {
+				count++
+			}
+			*prev = this
+		}
+	}
+	fmt.Println(count)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/asymmetricia/aoc21
+
+go 1.16
