d626c01d9c334028cdf387d92eb85893ad1ebd8a
asymmetricia <tricia-github@cernu.us>
2021-12-05 21:11:27 -0800

    day 6

--- day06/a/main.go
+++ day06/a/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
+	"strconv"
 	"strings"
 
 	"github.com/sirupsen/logrus"
 )
+
+const refrac = 6
+const child = 8
 
 var log = logrus.StandardLogger()
 
@@ -15,9 +20,27 @@ func main() {
 		log.WithError(err).Fatal("could not read input")
 	}
 
-	lines := strings.Split(strings.TrimSpace(string(input)), "\n")
-
-	for _, line := range lines {
-		line = line
+	//input = []byte("3,4,3,1,2")
+	fishS := strings.Split(strings.TrimSpace(string(input)), ",")
+	var fishes []int
+	for _, fish := range fishS {
+		f, _ := strconv.Atoi(fish)
+		fishes = append(fishes, f)
 	}
+
+	for day := 1; day <= 80; day++ {
+		next := make([]int, len(fishes))
+
+		for i, f := range fishes {
+			if f == 0 {
+				next[i] = refrac
+				next = append(next, child)
+			} else {
+				next[i] = f - 1
+			}
+		}
+		fishes = next
+		fmt.Println(day, fishes)
+	}
+	fmt.Println(len(fishes))
 }
--- day06/b/main.go
+++ day06/b/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
+	"strconv"
 	"strings"
 
 	"github.com/sirupsen/logrus"
 )
+
+const refrac = 6
+const child = 8
 
 var log = logrus.StandardLogger()
 
@@ -15,9 +20,38 @@ func main() {
 		log.WithError(err).Fatal("could not read input")
 	}
 
-	lines := strings.Split(strings.TrimSpace(string(input)), "\n")
-
-	for _, line := range lines {
-		line = line
+	//input = []byte("3,4,3,1,2")
+	fishS := strings.Split(strings.TrimSpace(string(input)), ",")
+	var fishes []int
+	for _, fish := range fishS {
+		f, _ := strconv.Atoi(fish)
+		fishes = append(fishes, f)
 	}
+
+	var ages = map[int]int{}
+	for _, fish := range fishes {
+		ages[fish]++
+	}
+
+	fmt.Println(ages)
+	for day := 1; day <= 256; day++ {
+		next := map[int]int{}
+		next[0] = ages[1]
+		next[1] = ages[2]
+		next[2] = ages[3]
+		next[3] = ages[4]
+		next[4] = ages[5]
+		next[5] = ages[6]
+		next[6] = ages[7]
+		next[7] = ages[8]
+		next[child] += ages[0]
+		next[refrac] += ages[0]
+		ages = next
+		fmt.Println(day, ages)
+	}
+	var t int
+	for _, count := range ages {
+		t += count
+	}
+	fmt.Println(t)
 }
