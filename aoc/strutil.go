7ea920801d3280b8c051d6faf7bc3f2d40852973
asymmetricia <tricia-github@cernu.us>
2021-12-12 11:09:30 -0800

    Split2 helper

--- aoc/strutil.go
+++ aoc/strutil.go
@@ -17,3 +17,11 @@ func After(haystack, needle string) string {
 	}
 	return haystack[pos+len(needle):]
 }
+
+func Split2(haystack, needle string) (string, string) {
+	pos := strings.Index(haystack, needle)
+	if pos < 0 {
+		return haystack, ""
+	}
+	return haystack[:pos], haystack[pos+len(needle):]
+}
