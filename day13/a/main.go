d8c67b9b34266c2cb31805046e04ff20f5af896e
asymmetricia <tricia-github@cernu.us>
2021-12-12 21:22:12 -0800

    day13

--- day13/a/main.go
+++ day13/a/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
+	"strconv"
 	"strings"
 
+	"github.com/asymmetricia/aoc21/aoc"
+	"github.com/asymmetricia/aoc21/coord"
 	"github.com/sirupsen/logrus"
 )
 
@@ -12,7 +16,7 @@ var log = logrus.StandardLogger()
 
 func main() {
 	input, err := ioutil.ReadFile("input")
-	// input, err := ioutil.ReadFile("test")
+	//input, err := ioutil.ReadFile("test")
 	if err != nil {
 		log.WithError(err).Fatal("could not read input")
 	}
@@ -20,7 +24,52 @@ func main() {
 
 	lines := strings.Split(strings.TrimSpace(string(input)), "\n")
 
+	var grid = map[coord.Coord]bool{}
+	type instruction struct {
+		isHoriz bool
+		val     int
+	}
+	var strux []instruction
+	var mode int
 	for _, line := range lines {
-		line = line
+		if line == "" {
+			mode++
+			continue
+		}
+		if mode == 0 {
+			grid[coord.MustFromComma(line)] = true
+		} else {
+			i, err := strconv.Atoi(aoc.After(line, "="))
+			if err != nil {
+				panic(err)
+			}
+			strux = append(strux, instruction{isHoriz: strings.Contains(line, "y="), val: i})
+		}
+	}
+
+	fmt.Println(len(grid))
+	for i, struc := range strux {
+		log.Println(struc)
+		after := map[coord.Coord]bool{}
+		if struc.isHoriz {
+			for c := range grid {
+				if c.Y < struc.val {
+					after[c] = true
+				} else {
+					after[coord.C(c.X, 2*struc.val-c.Y)] = true
+				}
+			}
+		} else {
+			for c := range grid {
+				if c.X < struc.val {
+					after[c] = true
+				} else {
+					after[coord.C(2*struc.val-c.X, c.Y)] = true
+				}
+			}
+		}
+		grid = after
+		log.Printf("%d: %d", i, len(grid))
+		break
 	}
 }
--- day13/b/main.go
+++ day13/b/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
+	"strconv"
 	"strings"
 
+	"github.com/asymmetricia/aoc21/aoc"
+	"github.com/asymmetricia/aoc21/coord"
 	"github.com/sirupsen/logrus"
 )
 
@@ -12,7 +16,7 @@ var log = logrus.StandardLogger()
 
 func main() {
 	input, err := ioutil.ReadFile("input")
-	// input, err := ioutil.ReadFile("test")
+	//input, err := ioutil.ReadFile("test")
 	if err != nil {
 		log.WithError(err).Fatal("could not read input")
 	}
@@ -20,7 +24,72 @@ func main() {
 
 	lines := strings.Split(strings.TrimSpace(string(input)), "\n")
 
+	var grid = map[coord.Coord]bool{}
+	type instruction struct {
+		isHoriz bool
+		val     int
+	}
+	var strux []instruction
+	var mode int
 	for _, line := range lines {
-		line = line
+		if line == "" {
+			mode++
+			continue
+		}
+		if mode == 0 {
+			grid[coord.MustFromComma(line)] = true
+		} else {
+			i, err := strconv.Atoi(aoc.After(line, "="))
+			if err != nil {
+				panic(err)
+			}
+			strux = append(strux, instruction{isHoriz: strings.Contains(line, "y="), val: i})
+		}
+	}
+
+	fmt.Println(len(grid))
+	for _, struc := range strux {
+		after := map[coord.Coord]bool{}
+		if struc.isHoriz {
+			for c := range grid {
+				if c.Y < struc.val {
+					after[c] = true
+				} else {
+					after[coord.C(c.X, 2*struc.val-c.Y)] = true
+				}
+			}
+		} else {
+			for c := range grid {
+				if c.X < struc.val {
+					after[c] = true
+				} else {
+					after[coord.C(2*struc.val-c.X, c.Y)] = true
+				}
+			}
+		}
+		grid = after
+	}
+
+	max := coord.Coord{0, 0}
+	for c := range grid {
+		if c.X > max.X {
+			max.X = c.X
+		}
+		if c.Y > max.Y {
+			max.Y = c.Y
+		}
+	}
+
+	fmt.Println(len(grid))
+	for y := 0; y <= max.Y; y++ {
+		s := ""
+		for x := 0; x <= max.X; x++ {
+			if grid[coord.C(x, y)] {
+				s += "#"
+			} else {
+				s += " "
+			}
+		}
+		log.Print(s)
 	}
 }
