b5cfe60cf05dbcd3926fec026527aa4f28d40ac1
asymmetricia <tricia-github@cernu.us>
2021-12-14 20:52:59 -0800

    day14

--- day14/a/main.go
+++ day14/a/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
 	"strings"
 
+	"github.com/asymmetricia/aoc21/aoc"
 	"github.com/sirupsen/logrus"
 )
 
@@ -12,7 +14,7 @@ var log = logrus.StandardLogger()
 
 func main() {
 	input, err := ioutil.ReadFile("input")
-	// input, err := ioutil.ReadFile("test")
+	//input, err := ioutil.ReadFile("test")
 	if err != nil {
 		log.WithError(err).Fatal("could not read input")
 	}
@@ -20,7 +22,46 @@ func main() {
 
 	lines := strings.Split(strings.TrimSpace(string(input)), "\n")
 
+	template := lines[0]
+	lines = lines[2:]
+	fmt.Println(template, lines)
+	var pairs = map[string]string{}
 	for _, line := range lines {
-		line = line
+		if line == "" {
+			continue
+		}
+		before := aoc.Before(line, " -> ")
+		after := aoc.After(line, " -> ")
+		pairs[before] = after
 	}
+
+	for step := 0; step < 10; step++ {
+		s := ""
+		for i := 0; i < len(template)-1; i++ {
+			if repl, ok := pairs[template[i:i+2]]; ok {
+				s += string(template[i])
+				s += repl
+			} else {
+				s += string(template[i])
+			}
+		}
+		s += string(template[len(template)-1])
+		fmt.Println(s)
+		template = s
+	}
+
+	var freq = map[rune]int{}
+	for _, c := range template {
+		freq[c]++
+	}
+	var min, max rune
+	for c, f := range freq {
+		if min == 0 || f < freq[min] {
+			min = c
+		}
+		if max == 0 || f > freq[max] {
+			max = c
+		}
+	}
+	fmt.Println(freq[max] - freq[min])
 }
--- day14/b/main.go
+++ day14/b/main.go
@@ -2,17 +2,77 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
 	"strings"
+	"time"
 
+	"github.com/asymmetricia/aoc21/aoc"
 	"github.com/sirupsen/logrus"
 )
 
 var log = logrus.StandardLogger()
 
+func countFreq(s string) map[string]int {
+	ret := map[string]int{}
+	for _, c := range s {
+		ret[string(c)]++
+	}
+	return ret
+}
+
+func sum(summands ...map[string]int) map[string]int {
+	ret := map[string]int{}
+	for _, summand := range summands {
+		for c, v := range summand {
+			ret[c] += v
+		}
+	}
+	return ret
+}
+
+var memo = map[struct {
+	string
+	int
+}]map[string]int{}
+
+func process(s string, steps int, pairs map[string]string) (inserts map[string]int) {
+	if memo, ok := memo[struct {
+		string
+		int
+	}{string: s, int: steps}]; ok {
+		return memo
+	}
+
+	pair, ok := pairs[s]
+	if !ok {
+		panic(pair)
+	}
+	if steps == 1 {
+		return map[string]int{pair: 1}
+	}
+
+	a := string(s[0]) + pair
+	b := pair + string(s[1])
+	indent := ""
+	for i := 10 - steps; i > 0; i-- {
+		indent += " "
+	}
+	ret := sum(
+		map[string]int{pair: 1},
+		process(a, steps-1, pairs),
+		process(b, steps-1, pairs))
+	memo[struct {
+		string
+		int
+	}{string: s, int: steps}] = ret
+	return ret
+}
+
 func main() {
+	start := time.Now()
 	input, err := ioutil.ReadFile("input")
-	// input, err := ioutil.ReadFile("test")
+	//input, err := ioutil.ReadFile("test")
 	if err != nil {
 		log.WithError(err).Fatal("could not read input")
 	}
@@ -20,7 +80,35 @@ func main() {
 
 	lines := strings.Split(strings.TrimSpace(string(input)), "\n")
 
+	template := lines[0]
+	lines = lines[2:]
+	fmt.Println(template, lines)
+	var pairs = map[string]string{}
 	for _, line := range lines {
-		line = line
+		if line == "" {
+			continue
+		}
+		before := aoc.Before(line, " -> ")
+		after := aoc.After(line, " -> ")
+		pairs[before] = after
 	}
+
+	freq := countFreq(template)
+	for i := 0; i < len(template)-1; i++ {
+		inserts := process(template[i:i+2], 40, pairs)
+		fmt.Println(template[i:i+2], "->", inserts)
+		freq = sum(freq, inserts)
+	}
+	fmt.Println(freq)
+	var min, max string
+	for c, f := range freq {
+		if min == "" || f < freq[min] {
+			min = c
+		}
+		if max == "" || f > freq[max] {
+			max = c
+		}
+	}
+	fmt.Println(freq[max] - freq[min])
+	fmt.Println(time.Since(start).Seconds())
 }
