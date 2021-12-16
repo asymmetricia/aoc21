a8e571f966c62fbfd3449cdab2d90efdba8806a3
asymmetricia <tricia-github@cernu.us>
2021-12-15 22:49:49 -0800

    day16 + bits

--- day16/a/main.go
+++ day16/a/main.go
@@ -2,17 +2,27 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
 	"strings"
 
+	"github.com/asymmetricia/aoc21/bits"
 	"github.com/sirupsen/logrus"
 )
 
 var log = logrus.StandardLogger()
 
+func SumVersions(p bits.Packet) int64 {
+	v := p.Version
+	for _, p := range p.Packets {
+		v += SumVersions(p)
+	}
+	return v
+}
+
 func main() {
 	input, err := ioutil.ReadFile("input")
-	// input, err := ioutil.ReadFile("test")
+	//input, err := ioutil.ReadFile("test")
 	if err != nil {
 		log.WithError(err).Fatal("could not read input")
 	}
@@ -20,7 +30,15 @@ func main() {
 
 	lines := strings.Split(strings.TrimSpace(string(input)), "\n")
 
-	for _, line := range lines {
-		line = line
+	decoded, err := bits.Decode(lines[0])
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	packet, _, err := bits.Parse(decoded)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(SumVersions(packet))
 }
--- day16/b/main.go
+++ day16/b/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
 	"strings"
 
+	"github.com/asymmetricia/aoc21/bits"
 	"github.com/sirupsen/logrus"
 )
 
@@ -12,7 +14,7 @@ var log = logrus.StandardLogger()
 
 func main() {
 	input, err := ioutil.ReadFile("input")
-	// input, err := ioutil.ReadFile("test")
+	//input, err := ioutil.ReadFile("test")
 	if err != nil {
 		log.WithError(err).Fatal("could not read input")
 	}
@@ -20,7 +22,14 @@ func main() {
 
 	lines := strings.Split(strings.TrimSpace(string(input)), "\n")
 
-	for _, line := range lines {
-		line = line
+	decoded, err := bits.Decode(lines[0])
+	if err != nil {
+		log.Fatal(err)
 	}
+	packet, _, err := bits.Parse(decoded)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(packet.String())
+	fmt.Println(bits.Execute(packet))
 }
