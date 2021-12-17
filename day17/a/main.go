dcb4dd4ef5cca756588b66c127fa7f2dab1a4630
asymmetricia <tricia-github@cernu.us>
2021-12-16 22:03:06 -0800

    day17

--- day17/a/main.go
+++ day17/a/main.go
@@ -2,13 +2,43 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
+	"regexp"
+	"strconv"
 	"strings"
 
 	"github.com/sirupsen/logrus"
 )
 
 var log = logrus.StandardLogger()
+
+func simulate(x, y, dx, dy int) (int, bool) {
+	var maxy = y
+	for {
+		if x > txb {
+			return 0, false
+		}
+		if y < tya {
+			return 0, false
+		}
+		if x >= txa && x <= txb && y <= tyb && y >= tya {
+			return maxy, true
+		}
+
+		x += dx
+		y += dy
+		if y > maxy {
+			maxy = y
+		}
+		if dx > 0 {
+			dx--
+		}
+		dy -= 1
+	}
+}
+
+var txa, txb, tya, tyb int
 
 func main() {
 	input, err := ioutil.ReadFile("input")
@@ -20,7 +50,41 @@ func main() {
 
 	lines := strings.Split(strings.TrimSpace(string(input)), "\n")
 
-	for _, line := range lines {
-		line = line
+	parts := regexp.
+		MustCompile(`target area: x=([0-9-]+)..([0-9-]+), y=([0-9-]+)..([0-9-]+)`).
+		FindStringSubmatch(lines[0])
+	if parts == nil {
+		panic(lines[0])
 	}
+
+	txa, err = strconv.Atoi(parts[1])
+	if err == nil {
+		txb, err = strconv.Atoi(parts[2])
+	}
+	if err == nil {
+		tya, err = strconv.Atoi(parts[3])
+	}
+	if err == nil {
+		tyb, err = strconv.Atoi(parts[4])
+	}
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(txa, txb, tya, tyb)
+
+	best := 0
+	count := 0
+	for dx := -1000; dx < 1000; dx++ {
+		for dy := -1000; dy < 1000; dy++ {
+			if maxy, ok := simulate(0, 0, dx, dy); ok {
+				if maxy > best {
+					best = maxy
+				}
+				count++
+			}
+		}
+	}
+	fmt.Println("(A) Highest Y:      ", best)
+	fmt.Println("(B) Total On Target:", count)
 }
